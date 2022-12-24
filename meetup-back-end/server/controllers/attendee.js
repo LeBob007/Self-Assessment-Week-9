d64c68917525c2edabe9740711812d42456bdcf6
Robert Hu <robert.y.hu.97@gmail.com>
2022-12-23 16:34:53 -0800

    Implement get and post

--- meetup-back-end/server/controllers/attendee.js
+++ meetup-back-end/server/controllers/attendee.js
@@ -2,8 +2,10 @@ const Attendee = require('../../db/models/Attendee');
 
 exports.getAll = (req, res) => {
   // your code here
+  Attendee.find().then(arr => res.status(200).send(arr)).catch(err => res.sendStatus(500))
 };
 
 exports.add = (req, res) => {
   // your code here
+  Attendee.create(req.body).then(arr => res.status(201).send(arr)).catch(err => res.sendStatus(500))
 };
